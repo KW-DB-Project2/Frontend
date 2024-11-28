6eb7d3e02679ba80cbddb6949a92cd052eb0f315
juyeonnnn <opff5423@gmail.com>
2024-11-28 23:54:36 +0900

    report & manage all product

--- src/userpages/Product.jsx
+++ src/userpages/Product.jsx
@@ -1,3 +1,13 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+// 아이콘
+import { FaExclamationTriangle, FaSearch, FaUserCircle } from 'react-icons/fa';
+import { FiLoader } from 'react-icons/fi';
+// 컴포넌트 임포트
+import { AuthContext } from '../context/AuthContext';
+
 function Product() {
   const { user, token } = useContext(AuthContext);
   const { productid } = useParams();
@@ -113,3 +123,145 @@ function Product() {
     </Container>
   );
 }
+
+export default Product;
+
+const Container = styled.div`
+  width: 1500px;
+  margin-top: 20px;
+  padding: 50px;
+  margin-top: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+const Image = styled.img`
+  width: 500px;
+  height: auto;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const Details = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+const ProductName = styled.h2`
+  font-size: 27px;
+  color: #333;
+  font-weight: 500;
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const Price = styled.div`
+  color: #333;
+  font-size: 33px;
+  font-weight: 500;
+`;
+
+const ReportButton = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border: none;
+  padding: 5px 10px;
+  font-size: 16px;
+  cursor: pointer;
+  color: red;
+  border-radius: 5px;
+
+  svg {
+    margin-right: 5px;
+  }
+`;
+
+const BottomBar = styled.div`
+  height: 2px;
+  width: 100%;
+  background-color: #f0f0f0;
+  margin: 20px 0px;
+`;
+
+const Description = styled.p`
+  flex-grow: 1;
+  margin-bottom: 100px;
+`;
+
+const BoardContent = styled.div`
+  display: flex;
+  justify-content: space-between; /* 왼쪽은 BoardUser, 오른쪽은 ButtonContainer */
+  align-items: center; /* 세로 정렬 */
+  width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px; /* 버튼들 사이 간격 */
+`;
+
+const BuyButton = styled.button`
+  padding: 10px;
+  background-color: #333;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 150px;
+  font-size: 20px;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+const QnAButton = styled.button`
+  padding: 10px;
+  background-color: white;
+  color: #ccc;
+  border: none;
+  cursor: pointer;
+  width: 150px;
+  align-self: flex-end;
+  font-size: 20px;
+`;
+
+const LoadingContainer = styled.div`
+  position: fixed; /* 화면에 고정 */
+  top: 50%; /* 화면의 세로 중앙 */
+  left: 50%; /* 화면의 가로 중앙 */
+  transform: translate(-50%, -50%); /* 정확히 중앙에 맞추기 위해 이동 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .loading-icon {
+    animation: rotate 1s linear infinite;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
