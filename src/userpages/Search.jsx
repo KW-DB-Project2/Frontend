74ae5b94559f388aa9ce5ee6781d7ecda12107af
juyeonnnn <opff5423@gmail.com>
2024-12-02 17:27:09 +0900

    product

--- src/userpages/Search.jsx
+++ src/userpages/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
